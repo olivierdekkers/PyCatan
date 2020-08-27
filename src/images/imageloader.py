f3a1ee150deff316582b47beaa63d2c9e1fe8983
odekkers <olivierdekkers@hotmail.com>
2020-08-27 20:51:23 +0200

    made imageloader a singelton

--- src/images/imageloader.py
+++ src/images/imageloader.py
@@ -9,10 +9,20 @@ import re
 
 class ImageLoader:
 
-    def __init__(self, pathToimages):
-        self._images = {}
-        for (dirpath, dirnames, filenames) in  os.walk(pathToimages):
-            self._images.update({filename.split('.')[0]: os.path.join(dirpath, filename) for filename in filenames if filename.endswith('png')})
+    instance = None
+    class _ImageLoader:
 
-    def __getattr__(self, imageName):
-        return pygame.image.load(self._images[imageName]).convert_alpha()
+        def __init__(self, pathToimages):
+            self._images = {}
+            for (dirpath, dirnames, filenames) in  os.walk(pathToimages):
+                self._images.update({filename.split('.')[0]: os.path.join(dirpath, filename) for filename in filenames if filename.endswith('png')})
+
+        def __getattr__(self, imageName):
+            return pygame.image.load(self._images[imageName]).convert_alpha()
+
+    def __init__(self, arg):
+        if not ImageLoader.instance:
+            ImageLoader.instance = _ImageLoader(arg)
+
+    def __getattr__(self, name):
+        return getattr(self.instance, name)
